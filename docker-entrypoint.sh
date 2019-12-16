e90f704639abd3e81f08cae79fefa9c0557fc5e7
Bnu - Service d'infrastructures informatique et numérique <55390243+bnu-siin@users.noreply.github.com>
2019-12-16 09:04:43 +0100

    refactor(script): docker-entrypoint.sh
    move run-parts option test.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,14 @@
   
 DIR=/docker-entrypoint.d
 
+if [ -f /etc/alpine-release ] ; then
+  OPTION=""
+else 
+  OPTION="--verbose"
+fi
+
 if [[ -d "$DIR" ]] ; then
-  if [ -f /etc/alpine-release ] ; then
-    OPTION=""
-  else 
-    OPTION="--verbose"
-  fi
+
   /bin/run-parts $OPTION "$DIR"
 fi
 
